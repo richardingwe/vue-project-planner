2362b9e0b7f245f6c126d5ae3f76074a2d7c587d
richardingwe <richardingwe5@gmail.com>
2021-07-06 22:40:49 +0100

    home view and data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,10 @@
 <template>
 	<div class="home">
-		Home
+		<div v-if="projects.length">
+			<div v-for="project in projects" :key="project.id">
+				<p>{{ project.title }}</p>
+			</div>
+		</div>
 	</div>
 </template>
 
@@ -8,5 +12,19 @@
 	export default {
 		name: 'Home',
 		components: {},
+		data() {
+			return {
+				projects: [],
+			};
+		},
+		async mounted() {
+			try {
+				const res = await fetch('http://localhost:3000/projects');
+				const data = await res.json();
+				this.projects = data;
+			} catch (error) {
+				console.log(error);
+			}
+		},
 	};
 </script>
