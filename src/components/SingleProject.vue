ea2b1976fea16f9b0df87f4b5eaa701552df1d25
richardingwe <richardingwe5@gmail.com>
2021-07-07 14:30:51 +0100

    worked on the delete project method

--- src/components/SingleProject.vue
+++ src/components/SingleProject.vue
@@ -4,7 +4,7 @@
 			<h3 @click="toggleShowDetails">{{ project.title }}</h3>
 			<div class="icons">
 				<span class="material-icons">edit</span>
-				<span class="material-icons">delete</span>
+				<span @click="deleteProject" class="material-icons">delete</span>
 				<span class="material-icons tick">done</span>
 			</div>
 		</div>
@@ -20,11 +20,20 @@
 		data() {
 			return {
 				showDetails: false,
+				uri: 'http://localhost:3000/projects/' + this.project.id,
 			};
 		},
 		methods: {
 			toggleShowDetails() {
 				this.showDetails = !this.showDetails;
+			},
+			async deleteProject() {
+				try {
+					await fetch(this.uri, { method: 'DELETE' });
+					this.$emit('delete', this.project.id);
+				} catch (error) {
+					console.log(error);
+				}
 			},
 		},
 	};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
 	<div class="home">
 		<div v-if="projects.length">
 			<div v-for="project in projects" :key="project.id">
-				<SingleProject :project="project" />
+				<SingleProject :project="project" @delete="handleDelete" />
 			</div>
 		</div>
 	</div>
@@ -26,6 +26,11 @@
 			} catch (error) {
 				console.log(error);
 			}
+		},
+		methods: {
+			handleDelete(id) {
+				this.projects = this.projects.filter((project) => project.id !== id);
+			},
 		},
 	};
 </script>
